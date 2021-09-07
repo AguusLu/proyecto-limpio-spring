f8235292dcec42b65ceb411f777f2fdf8d848d1f
Agus <agus.luu@gmail.com>
2021-09-07 15:40:08 -0300

    CajaFuerte

--- src/main/java/ar/edu/unlam/tallerweb1/tdd/CajaFuerte.java
+++ src/main/java/ar/edu/unlam/tallerweb1/tdd/CajaFuerte.java
@@ -2,5 +2,25 @@ package ar.edu.unlam.tallerweb1.tdd;
 
 public class CajaFuerte {
 
+    private Boolean abierta = true;
 
+    public boolean estaAbierta(){
+        return abierta;
+    }
+
+    public void cerrar() {
+        abierta = false;
+    }
+
+    public void cerrar(int codigoDeApertura) {
+        abierta = false;
+    }
+
+    public boolean estaCerrada() {
+        return !estaAbierta();
+    }
+
+    public void abrir(int codigoDeApertura) {
+        abierta = true;
+    }
 }
--- src/test/java/ar/edu/unlam/tallerweb1/tdd/CerraduraTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/tdd/CerraduraTest.java
@@ -8,6 +8,8 @@ import static org.assertj.core.api.Assertions.*;
 
 public class CerraduraTest {
 
+    public static final int CODIGO_DE_APERTURA = 3434;
+
     @Test
     public void cuandoSeCreaLaCajaFuerteDebeEstarAbierta(){
         //preparacion Given(dado)
@@ -16,14 +18,68 @@ public class CerraduraTest {
         CajaFuerte caja = whenCreoUnaCajaFuerte();
 
         //comprobacion Then(entonces)
-        thenLaCajaFuerteEstaAbierta();
+        thenLaCajaFuerteEstaAbierta(caja);
     }
 
-    private void thenLaCajaFuerteEstaAbierta() {
+   /* @Test
+    public void alCerrarLaCajaFuerteDebeEstarCerrada(){
+        CajaFuerte caja = givenCreoUnaCajaFuerte();
+
+        whenCierroLaCajaFuerte(caja);
+        caja.cerrar();
+
+        thenLaCajaFuerteEstaCerrada(caja);
+    }*/
+
+    @Test
+    public void alIngresarElCodigoCorrectoLaCajaSeAbre(){
+        CajaFuerte caja = givenCreoUnaCajaFuerte();
+
+        caja.cerrar(CODIGO_DE_APERTURA);
+
+        whenAbroLaCajaFuerteCon(CODIGO_DE_APERTURA, caja);
+        
+        thenLaCajaFuerteEstaAbierta(caja);
+    }
+
+    private void whenAbroLaCajaFuerteCon(int codigoDeApertura, CajaFuerte caja) {
+        caja.abrir(CODIGO_DE_APERTURA);
+    }
+
+    @Test
+    public void alCerrarConCodigoLaCajaFuerteDebeEstarCerrada(){
+        CajaFuerte caja = givenCreoUnaCajaFuerte();
+
+        caja.cerrar(CODIGO_DE_APERTURA);
+
+        thenLaCajaFuerteEstaCerrada(caja);
+    }
+
+    private CajaFuerte givenCreoUnaCajaFuerte() {
+        return getCajaFuerte();
+    }
+
+    private CajaFuerte getCajaFuerte() {
+        return crearCaja();
+    }
+
+    private void whenCierroLaCajaFuerte(CajaFuerte caja) {
+        caja.cerrar();
+    }
+
+    private void thenLaCajaFuerteEstaCerrada(CajaFuerte caja) {
+        assertThat(caja.estaCerrada()).isTrue();
+    }
+
+    private void thenLaCajaFuerteEstaAbierta(CajaFuerte caja) {
         assertThat(caja.estaAbierta()).isTrue();
     }
 
     private CajaFuerte whenCreoUnaCajaFuerte(){
+        return getCajaFuerte();
+    }
+
+    private CajaFuerte crearCaja(){
         return new CajaFuerte();
     }
 }
